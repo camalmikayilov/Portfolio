91342445dc0f9aabb335a206952194410cd751a0
camalmikayilov <camalmikayilov1@gmail.com>
2023-08-22 10:06:31 +0400

    last

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Jamal Mikayilov</h1>
-        <h5 className="text-light">Front-end Developer</h5>
+        <h5 className="text-light">Web Developer</h5>
         <CTA />
         <a href="#contact" className="scroll__down">
           Scroll Down
